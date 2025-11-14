dd2bb4efeb0541738b5a2c719d9fa0ea213dd1b8
DianAnto2103 <dianaantonela2103@gmail.com>
2025-11-13 23:23:00 -0500

    Commit24_ModeloCliente
    Se ha creado la clase modelo de Cliente, este solo con getters y setters, con la finalidad de escribir los datos del clientes. Por el momento se piensa relacionarlo con el patrón repository. Este no pertenece a ningun patrón de diseño aún.

--- mavenproject1/src/main/java/model/Cliente.java
+++ mavenproject1/src/main/java/model/Cliente.java
@@ -9,6 +9,50 @@ package model;
  * @author diana
  */
 public class Cliente {
+    private int ID;
+    private String RUC;
+    private String telefono;
+    private String razonSocial;
+    private double mensualidad;
+   
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
     
+    public String getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRazon_social() {
+        return razonSocial;
+    }
+
+    public void setRazon_social(String razon_social) {
+        this.razonSocial = razon_social;
+    }
+
+    public double getMensualidad() {
+        return mensualidad;
+    }
+
+    public void setMensualidad(double mensualidad) {
+        this.mensualidad = mensualidad;
+    }
     
 }
