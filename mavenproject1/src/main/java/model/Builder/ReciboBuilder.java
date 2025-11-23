c2b200b61ccba9d0668bb48b9a9c128f9671c585
DarienCbrCode <142682969+DarienCbrCode@users.noreply.github.com>
2025-11-22 22:58:14 -0500

    model.builder: recibo builder(terminado)

--- mavenproject1/src/main/java/model/Builder/ReciboBuilder.java
+++ mavenproject1/src/main/java/model/Builder/ReciboBuilder.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.Builder;
 
-/**
- *
- * @author USER
- */
-public class ReciboBuilder {
-    
+public interface ReciboBuilder {
+
+    ReciboBuilder setCliente(String cliente);
+
+    ReciboBuilder setConcepto(String concepto);
+
+    ReciboBuilder setMonto(double monto);
+
+    Recibo build();   // RETORNA EL RECIBO LISTO
 }
