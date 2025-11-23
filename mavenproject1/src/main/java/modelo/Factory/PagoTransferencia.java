0c741918239cf9c35b6787d06a7a51fe1a0371f9
DarienCbrCode <142682969+DarienCbrCode@users.noreply.github.com>
2025-11-22 22:01:11 -0500

    metodo de pago: transferencia(terminado)

--- mavenproject1/src/main/java/modelo/Factory/PagoTransferencia.java
+++ mavenproject1/src/main/java/modelo/Factory/PagoTransferencia.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo.Factory;
 
-/**
- *
- * @author diana
- */
-public class PagoTransferencia {
+public class PagoTransferencia implements MetododePago {
     
+    public void procesarPago(double monto) {
+        System.out.println("Pago por TRANSFERENCIA registrado: S/ " + monto);
+    }
 }
