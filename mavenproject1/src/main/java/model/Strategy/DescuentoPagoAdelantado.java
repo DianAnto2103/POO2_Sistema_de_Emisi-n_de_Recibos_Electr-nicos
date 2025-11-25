d97c6e89a6090f20faa4864a566122188cf56b08
flavia1906 <flavia.retuerto@usil.pe>
2025-11-24 21:42:56 -0500

    Commit83_ Descuento del 5%

--- mavenproject1/src/main/java/model/Strategy/DescuentoPagoAdelantado.java
+++ mavenproject1/src/main/java/model/Strategy/DescuentoPagoAdelantado.java
@@ -9,23 +9,12 @@ import model.Cliente;
  * @author Flavia
  */
 public class DescuentoPagoAdelantado implements CalculoStrategy {
-    
-    private static final double PORCENTAJE_DESCUENTO = 0.05; // 5%
+private static final double PORCENTAJE_DESCUENTO = 0.05; // 5%
     
    @Override
     public double calcularMontoFinal(double montoBase, Cliente cliente) {
         double descuento = montoBase * PORCENTAJE_DESCUENTO;
         double montoFinal = montoBase - descuento;
-        
-        System.out.println("----------------------------------");
-        System.out.println("  DESCUENTO POR PAGO ADELANTADO");
-        System.out.println("----------------------------------");
-        System.out.println(String.format("  Cliente: %s", cliente.getRazonSocial()));
-        System.out.println(String.format("  Monto base: S/ %.2f", montoBase));
-        System.out.println(String.format("  Descuento (5%%): S/ %.2f", descuento));
-        System.out.println(String.format("  Monto final: S/ %.2f", montoFinal));
-        System.out.println(String.format("  ¡Ahorro: S/ %.2f!", descuento));
-        System.out.println("----------------------------------\n");
         
         return montoFinal;
     }
